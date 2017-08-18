5112ec120c71c96ad66e5b51178a47ea0e0d5aa5
jessicablois <jblois@ucmerced.edu>
2017-08-18 11:40:15 -0700

    working with stringing pipes together

--- 2017-08-18-script.R
+++ 2017-08-18-script.R
@@ -19,5 +19,18 @@ surveys_trimmed <- surveys %>%   # this is the input...ie, take surveys as the i
   filter(year == 1995) %>% # put surveys into filter
   select(plot_id, species_id, weight) # then take the output of the previous line and put it into select
   
-  
+# add a new column to surveys, weight_kg  
+surveys %>%
+  filter(!is.na(weight)) %>%
+  mutate(weight_kg = weight / 1000,
+         weight_kg2 = weight_kg *2) %>%
+  head
 
+
+# Challenge:
+
+surveys %>%
+  mutate(hfl0.5 = hindfoot_length / 2) %>%
+  filter(!is.na(hfl0.5)) %>%
+  select(species_id, hfl0.5) %>%
+  tail
